d6f137006cf6220b3397153498e1bd2ca166c2ca
Somto ZIkora <zikorasomto@gmail.com>
2019-08-02 20:44:16 +0200

    Section 6: Train the  model

--- script.js
+++ script.js
@@ -60,6 +60,29 @@ function convertToTensor(data) {
   })
 }
 
+async function trainModel(model, inputs, labels) {
+  console.log('Preparing the model for training')
+  model.compile({
+    optimizer: tf.train.adam(),
+    loss: tf.losses.meanSquaredError,
+    metrics: ['mse']
+  })
+
+  const batchSize = 32;
+  const epochs = 50
+
+  return await model.fit(inputs, labels, {
+    batchSize,
+    epochs,
+    shuffle: true,
+    callbacks: tfvis.show.fitCallbacks(
+      { name: 'Training Performance' },
+      ['loss', 'mse'],
+      { height: 200, callbacks: ['onEpochEnd'] }
+    )
+  })
+}
+
 async function run() {
   console.log('Running main function')
   const data = await getData()
